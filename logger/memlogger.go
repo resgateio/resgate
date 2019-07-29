51c36534262e6a2daad5b3915ff03d5998407b03
Samuel Jirénius <samuel@jirenius.com>
2019-07-29 15:31:34 +0200

    GH-93: Fixed memlogger race condition.

--- logger/memlogger.go
+++ logger/memlogger.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"sync"
 )
 
 // MemLogger writes log messages to os.Stderr
@@ -12,6 +13,7 @@ type MemLogger struct {
 	b     *bytes.Buffer
 	debug bool
 	trace bool
+	mu    sync.Mutex
 }
 
 // NewMemLogger returns a new logger that writes to a bytes buffer
@@ -33,24 +35,32 @@ func NewMemLogger(debug bool, trace bool) *MemLogger {
 
 // Logf writes a log entry
 func (l *MemLogger) Logf(prefix string, format string, v ...interface{}) {
+	l.mu.Lock()
 	l.log.Print(prefix, fmt.Sprintf(format, v...))
+	l.mu.Unlock()
 }
 
 // Debugf writes a debug entry
 func (l *MemLogger) Debugf(prefix string, format string, v ...interface{}) {
 	if l.debug {
+		l.mu.Lock()
 		l.log.Print(prefix, fmt.Sprintf(format, v...))
+		l.mu.Unlock()
 	}
 }
 
 // Tracef writes a trace entry
 func (l *MemLogger) Tracef(prefix string, format string, v ...interface{}) {
 	if l.trace {
+		l.mu.Lock()
 		l.log.Print(prefix, fmt.Sprintf(format, v...))
+		l.mu.Unlock()
 	}
 }
 
 // String returns the log
 func (l *MemLogger) String() string {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	return l.b.String()
 }
