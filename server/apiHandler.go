5661b8b865a54bdec043fa5c2ee092068217fae0
Guilherme Belmonte <contact@belmonte.dev>
2020-08-31 11:24:13 -0300

    Fix CORS for requests with content-type header

--- server/apiHandler.go
+++ server/apiHandler.go
@@ -30,6 +30,7 @@ func (s *Service) initAPIHandler() error {
 // setCommonHeaders sets common headers such as Access-Control-*.
 // It returns error if the origin header does not match any allowed origin.
 func (s *Service) setCommonHeaders(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Access-Control-Allow-Headers", "content-type")
 	if s.cfg.allowOrigin[0] == "*" {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		return nil
