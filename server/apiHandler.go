abcbec94a7ca04c52596db61df4a4538e61a58d3
Guilherme Belmonte <contact@belmonte.dev>
2020-09-02 17:33:21 -0300

    Change allowed headers

--- server/apiHandler.go
+++ server/apiHandler.go
@@ -32,7 +32,7 @@ func (s *Service) initAPIHandler() error {
 func (s *Service) setCommonHeaders(w http.ResponseWriter, r *http.Request) error {
 	if s.cfg.allowOrigin[0] == "*" {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "content-type")
+		w.Header().Set("Access-Control-Allow-Headers", "content-type, authorization")
 		return nil
 	}
 
@@ -43,12 +43,12 @@ func (s *Service) setCommonHeaders(w http.ResponseWriter, r *http.Request) error
 	if len(origin) > 0 && origin[0] != "null" {
 		if matchesOrigins(s.cfg.allowOrigin, origin[0]) {
 			w.Header().Set("Access-Control-Allow-Origin", origin[0])
-			w.Header().Set("Access-Control-Allow-Headers", "content-type")
+			w.Header().Set("Access-Control-Allow-Headers", "content-type, authorization")
 			w.Header().Set("Vary", "Origin")
 		} else {
 			// No matching origin
 			w.Header().Set("Access-Control-Allow-Origin", s.cfg.allowOrigin[0])
-			w.Header().Set("Access-Control-Allow-Headers", "content-type")
+			w.Header().Set("Access-Control-Allow-Headers", "content-type, authorization")
 			w.Header().Set("Vary", "Origin")
 			return reserr.ErrForbiddenOrigin
 		}
