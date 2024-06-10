2b1102ce6c87401caec6dbdc7a2fb203c9bff968
Samuel Jirénius <samuel@jirenius.com>
2024-06-10 08:41:39 +0200

    GH-245: Sending test coverage to coveralls with goveralls. Removed golint.

--- scripts/install-checks.sh
+++ scripts/install-checks.sh
@@ -3,6 +3,5 @@
 pushd /tmp > /dev/null
 go get -u github.com/mattn/goveralls
 go get -u honnef.co/go/tools/cmd/staticcheck
-go get -u golang.org/x/lint/golint
 go get -u github.com/client9/misspell/cmd/misspell
 popd > /dev/null
