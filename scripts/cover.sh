dca3e92962951cabf5c3f5d81195246fa36bebc6
Samuel Jirénius <samuel@jirenius.com>
2024-06-30 15:48:05 +0200

    GH-253: Run all tests for build and cover scripts.

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 # Run from directory above via ./scripts/cover.sh
 
-go test -v -covermode=atomic -coverprofile=./cover.out -coverpkg=./server/... ./test
+go test -v -covermode=atomic -coverprofile=./cover.out -coverpkg=./server/... ./...
 go tool cover -html=cover.out
